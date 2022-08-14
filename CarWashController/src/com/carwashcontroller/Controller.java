576cdb75bd5588893b3692ffdbe5260c67a2724f
egecitak <egecitak01@gmail.com>
2022-08-14 23:02:17 +0300

    Comments are done, executable .jar file is created

--- CarWashController/src/com/carwashcontroller/Controller.java
+++ CarWashController/src/com/carwashcontroller/Controller.java
@@ -9,6 +9,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Controller extends JFrame {
+    //All GUI elements are placed with the help of GUI designer of IntelliJ Idea
     private JPanel wrapper;
     private JPanel topLeft;
     private JPanel topRight;
@@ -56,11 +57,13 @@ public class Controller extends JFrame {
 
     public Controller() {
 
+        //All radio buttons are grouped to prevent editing from the user
         for (int i = 0; i < 9; i++)
         {
             bg.add(rbtnArr[i]);
         }
 
+        //Start button action listener, which resets stopFlag and starts washing
         btn_start.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -69,6 +72,7 @@ public class Controller extends JFrame {
             }
         });
 
+        //Stop button action listener, which sets stopFlag and cuts washing in the middle
         btn_stop.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -78,6 +82,8 @@ public class Controller extends JFrame {
     }
 
     public static void main(String[] args) {
+
+        //Main function is only used for GUI
         JFrame frame = new JFrame("Car Wash Controller");
         frame.setContentPane(new Controller().wrapper);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -113,6 +119,7 @@ public class Controller extends JFrame {
                 actOps++;
             }
         }
+        //If no check box is selected, select first one and increment active ops counter
         if (emptySelection) {
             chbx1.setSelected(true);
             actOps++;
@@ -142,21 +149,21 @@ public class Controller extends JFrame {
 
             if (tmpChbx.isSelected())
             {
+                //Creating operation objects only for active operations, they also hold their station and corresponding LED
                 Operation operation = new Operation(tmpChbx.getText(), true, station, 5, tmpRbtn);
                 opArr[index] = operation;
                 index++;
             }
         }
 
+        //To disable the checkboxes, enable slider and stop button
         for (JCheckBox j : chbxArr) {
             j.setEnabled(false);
         }
-
-        //To disable the checkboxes, enable slider and stop button
         sld.setEnabled(true);
         btn_stop.setEnabled(true);
 
-
+        //The timer is necessary to create a buffet between start command and process, since otherwise program crashes
         Timer timer = new Timer();
         TimerTask taskDelay = new TimerTask() {
             @Override
@@ -165,32 +172,40 @@ public class Controller extends JFrame {
             }
         };
 
-        timer.schedule(taskDelay, 500);
+        timer.schedule(taskDelay, 200); //runs once, creates 200 ms buffer
 
     }
 
     public void washing (Operation[] opArr) {
 
+        //Iterates over active operations array
         for (int i = 0; i < opArr.length; i++)
         {
+            //Check timer is necessary for slider position checking
             Timer checkTimer = new Timer();
+            //Wash timer is to keep track of the elapsed time
             Timer washTimer = new Timer();
 
             Operation activeOp = new Operation();
             activeOp = opArr[i];
 
+            //These all defined like this to use in the inner classes of TimerTasks
             int[] count = {-1};
-            int[] counterLimit = {activeOp.getTime()};
-
-            boolean firstRun = true;
+            int[] counterLimit = {activeOp.getTime()}; //required time for an operation
             boolean[] washOK = new boolean[1];
-            washOK[0] = ledChecker(activeOp);
+
+            //Initial flags to seperate the first iterations
+            boolean firstRun = true;
             boolean started = false;
+
+            //Initial slider position checker
+            washOK[0] = ledChecker(activeOp);
 
             int startCount = 0;
 
             while (activeOp.isDone() == false) {
 
+                //If stop button is clicked, the timers are cancelled then function exits
                 if (stopFlag)
                 {
                     checkTimer.cancel();
@@ -209,6 +224,7 @@ public class Controller extends JFrame {
                     }
                 };
 
+                //Starting check timer during first run for every active operation
                 if (firstRun) {
                     checkTimer.schedule(checkTask, 0, 100);
                     firstRun = false;
@@ -224,13 +240,13 @@ public class Controller extends JFrame {
                     }
                 };
 
-                //Washing start condition
+                //Washing start condition which also starts timer
                 if (washOK[0] && !started) {
                     washTimer.schedule(washTask, startCount, 1000);
                     started = true;
                 }
 
-                //Washing done
+                //Washing done, timers are cancelled, operation's "LED" is disabled and it is marked as done
                 if (count[0] == counterLimit[0]) {
                     checkTimer.cancel();
                     washTimer.cancel();
@@ -241,17 +257,20 @@ public class Controller extends JFrame {
             }
         }
 
+        //After a normal process, keep out of pos. LED open until slider is in Exit
         while (sld.getValue() != 4)
         {
             rbtn9.setEnabled(true);
             rbtn9.setSelected(true);
         }
 
+        //Then stop
         stop();
     }
 
     public void stop() {
 
+        //Elapsed time text is reset, LEDs are back to their initial states
         txtFld_time.setText("0.00");
         rbtn9.setEnabled(false);
         bg.clearSelection();
@@ -259,14 +278,14 @@ public class Controller extends JFrame {
         //Wash Vacant indicator
         txtFld_washvacant.setBackground(Color.GREEN);
         txtFld_washvacant.setText("Wash Vacant");
+
+        //Start button enabled
         btn_start.setEnabled(true);
 
-        //Enabling checkboxes
+        //To enable the checkboxes and to disable slider and stop button
         for (JCheckBox j : chbxArr) {
             j.setEnabled(true);
         }
-
-        //To enable the checkboxes and to disable slider and stop button
         sld.setEnabled(false);
         sld.setValue(0);
         btn_stop.setEnabled(false);
@@ -275,6 +294,8 @@ public class Controller extends JFrame {
 
     public boolean ledChecker(Operation operation) {
 
+        /*Checks if the slider position is in accordance with the station required by the operation, if not
+        open out of pos LED. */
         if (operation.getStation() != sld.getValue()) {
             operation.getRbtn().setSelected(false);
             operation.getRbtn().setEnabled(false);
